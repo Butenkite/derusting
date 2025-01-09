d86b64bc1dc455f5447fa5beff103a01a4748dde
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-01-09 12:07:11 -0800

    Part of ArrayList submitted.

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList; // needs to be imported to run
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    ArrayList<String> list = new ArrayList<String>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+    list.add("A");
+    list.add("B");
+    list.add("C");
     // Print the element at index 1
-
+    System.out.println(list.get(1));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
 
