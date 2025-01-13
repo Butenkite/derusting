9d0d7ca1f44722bca28979a638f04c547ef288d1
unknown <174657090+Butenkite@users.noreply.github.com>
2025-01-13 15:29:28 -0800

    Complete Number Practice.

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,18 +1,26 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
-
+    float i = -0.1f;
     // Create an int with a positive value and assign it to a variable
-
+    int k = 4;
     // Use the modulo % operator to find the remainder when the int is divided by 3
-
+    int l = k%3;
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if(k % 2 == 1){
+      System.out.println("Odd");
+    }
+    else{
+      System.out.println("Even");
+    }
 
     // Divide the number by another number using integer division
-
+    int a = 4;
+    int b = 2;
+    int c = a / b;
     /*
      * Reminder!
      * 
