c4a40b51bbc45f7155ca7573348a016aff2fe1ca
unknown <174657090+Butenkite@users.noreply.github.com>
2025-01-13 18:38:23 -0800

    set practice complete.

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,18 +1,24 @@
+import java.util.*;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
-
+    Set<String> hashSetPractice = new HashSet<String>();
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
-
+    hashSetPractice.add("good morning");
+    hashSetPractice.add("good afternoon");
+    hashSetPractice.add("good night");
     // Check whether the Set contains a given String
-
+    hashSetPractice.contains("good morning");
     // Remove an element from the Set
-
+    hashSetPractice.remove("good night");
     // Get the size of the Set
-
+    hashSetPractice.size();
     // Iterate over the elements of the Set, printing each one on a separate line
-
+    for(String saying : hashSetPractice){
+      System.out.println(saying);
+    }
     /*
      * Warning!
      * 
